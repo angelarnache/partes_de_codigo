b81393562679e32e414f046135174ae392d6a82a
angelarnache <adavicde@gmail.com>
2026-03-11 13:23:22 -0500

    Update and rename angelprueba2.py to partes_de_codigo.py

--- angelprueba2.py
+++
@@ -1,3 +0,0 @@
-print("hola mundo")
-print("esto es nuevo")
-print ("pureba")
---
+++ partes_de_codigo.py
@@ -0,0 +1,17 @@
+status = "si"
+
+while status == "si":
+    data = input("Ingrese un nombre: ")
+    print (data)
+    status = input("para continuar escriba si de lo contrarion escriba no: ")
+
+#names=["angel","pedro","jose","maria","pedro","jorgee"]
+
+#def saludar(names):
+#    mensaje=f"hola {names}"
+#    mensaje2="adios"
+#    return mensaje,mensaje2
+
+#for name in names:
+#    saludo=saludar(name)
+#    print(f"{saludo[0]} {saludo[1]}")
