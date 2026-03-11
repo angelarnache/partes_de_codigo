06781a58c81ad9931535779b83b3eaf88360557b
angelarnache <adavicde@gmail.com>
2026-03-11 13:46:47 -0500

    Update partes_de_codigo.py

--- partes_de_codigo.py
+++ partes_de_codigo.py
@@ -4,17 +4,18 @@ while status == "si":
     data = input("Ingrese un nombre: ")
     print (data)
     status = input("para continuar escriba si de lo contrarion escriba no: ")
-
+______________________________________________
 names=["angel","pedro","jose","maria","pedro","jorgee"]
 
 def saludar(names):
     mensaje=f"hola {names}"
     mensaje2="adios"
     return mensaje,mensaje2
-
+_______________________________________
 for name in names:
     saludo=saludar(name)
     print(f"{saludo[0]} {saludo[1]}")
+__________________________________________________
     LIST = []
 for i in range(3):
     palabra = input("enter a word: ")
