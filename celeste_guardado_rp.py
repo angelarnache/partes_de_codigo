3eb0dd8365a7d5314548e4879b23db961ae8b0ec
Sebastian Arnache Cantillo <sarnachecantillo@gmail.com>
2026-03-12 20:07:48 -0500

    feat: Working on it

--- celeste_guardado_rp.py
+++ celeste_guardado_rp.py
@@ -1,27 +1,26 @@
 def register_products(quantity_in=1):
-    product =[]
-    amount=[]
-    cost=[]
+    products =[]
+    amounts=[]
+    costs=[]
     for i in range(quantity_in):
         print(f"\n────product {i+1}─────")
         word=input("Enter product: ")
         word_2=int(input("Enter quantity: "))
         word_3=float(input("Enter the product cost: "))
-        product.append(word)
-        amount.append(word_2)
-        cost.append(word_3)
+        products.append(word)
+        amounts.append(word_2)
+        costs.append(word_3)
     
-        
-        resultado="yes"
-        while resultado == "yes":
-            resultado = input("Do you want to add more products?: ")
-        if resultado =="yes":
-            return register_products()
-        else:
-            print("gracias")
-            break
-    return product,amount,cost
+        total = [ x * y for x, y in zip(costs, amounts)]
+    print(total)
+    return total
+resultado="yes"
 
-a=register_products
-print("listo")
-print(a)
+while resultado == "yes":
+    resultado = input("Do you want to add more products?: ")
+    if resultado =="yes":
+        register_products()
+    else:
+        print("gracias")
+        break
+
